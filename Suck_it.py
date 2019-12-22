6d70e424691eb7a4152111fac81e9e2501c46464
James <30578100+jamesssf@users.noreply.github.com>
2019-12-22 07:15:11 -0800

    updating suck it

--- Suck_it.py
+++ Suck_it.py
@@ -1,11 +1,15 @@
 def suck_it()
 
-    sample_counter = 0                  # Sample counter. Counts four samples
+    sample_counter = 0                  # Sample counter. Counts four samples. This needs to come from main loop
     flushed_flag = 0                    # Set to 1 after flush. Flush before each sample. reset to 0 when finished_pump = 1
     finished_pump = 0                   # Set to 1 only when done, in case interrupt before finish filling sample
 
     while in_position:                  # in_position variable checks location of USV against the target GPS location
         if flushed_flag:
+            sample = samplePins[sample_counter]
+            GPIO.out(pump_pin, LOW)  # Turn the pump on
+            GPIO.out(sample, LOW)    # Turn the specific solenoid on given by sample variable
+            while finished_pump:
 
         else:
             flush()
@@ -19,3 +23,5 @@ def flush():
     GPIO.out(pinList, HIGH)          # Close all relays, stop the pump and close flush valve
     flushed_flag = 1                 # Set flag to 1 so program doesn't flush twice on one sample
 
+def waterflow():
+    
